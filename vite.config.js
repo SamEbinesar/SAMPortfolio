0ba79c727379178544132ef68524da1ba397bd8a
SamEbinesar <samebinesar2004@gmail.com>
2025-11-15 10:53:35 +0530

    vite fixed

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: '/',
+    server: {
+      port: 3000,
+      open: true,
+      proxy: mode === 'development' ? {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false
+        }
+      } : undefined
     }
   }
 })
-
